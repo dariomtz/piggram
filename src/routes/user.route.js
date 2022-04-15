c019f45b32e7b92be7610ee8fa6caeef8030ede2
Armando Ferreira <is722820@iteso.mx>
2022-04-15 16:41:42 -0500

    Modifications to the user route

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,1 +1,24 @@
-//Por hacer XXD
+const router = require('express').Router()
+const bcryptjs = require('bcryptjs')
+const moment = require('moment')
+const User = require('../models/schemas/user')
+
+router.post('/signup',(req, res)=>{
+
+})
+router.post('/login',(req, res)=>{
+
+})
+router.post('/logout',(req, res)=>{
+
+})
+router.get('/user/:id',(req, res)=>{
+
+})
+router.put('/user/:id',(req, res)=>{
+
+})
+router.delete('/user/:id',(req, res)=>{
+
+})
+
