d11db0ee43d9d3e09b3388e49d3022f5e23e9c13
Armando Ferreira <is722820@iteso.mx>
2022-05-06 20:53:41 -0500

    Multer in Post controller and Post route

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,39 +1,41 @@
 const router = require("express").Router();
-const bcryptjs = require("bcryptjs");
-const moment = require("moment");
 const userController = require("../controllers/user.controller");
-const { handleErrorAsync } = require("../utils/hof");
 const { uploadLocal } = require("../utils/multer");
+const { isAuthenticated } = require("../midlewares/auth.midleware");
+
+router.use(isAuthenticated);
 
 router.post(
-  "/profilePictureLocal",
+  "/:id/profilePictureLocal",
   uploadLocal.single("profilePicture"),
   (req, res) => {
+    const { id } = req.params;
     console.log(req.file);
     const path = req.file.path.replace("src\\", "");
     console.log(path);
-    userController.saveProfilePicture(`http://localhost:3000/${path}`); // Modificar la url
+    userController.saveProfilePicture(`${id},http://localhost:3000/${path}`); // Modificar la url
   }
 );
 
-router.get("/getProfilePicture", (req, res) => {
-  res.send({ url: userController.getProfilePicture() });
+router.get("/:id/getProfilePicture", isAuthenticated, (req, res) => {
+  const { id } = req.params;
+  res.send({ url: userController.getProfilePicture(id) });
 });
 
 router.get("/", (req, res) => {
   res.send({ user: userController.list() });
 });
-router.get("/:id", handleErrorAsync, (req, res) => {
+router.get("/:id", isAuthenticated, (req, res) => {
   const { id } = req.params;
   const user = userController.getById(id);
   res.send(user);
 });
-router.put("/:id", handleErrorAsync, (req, res) => {
+router.put("/:id", isAuthenticated, (req, res) => {
   const { id } = req.params;
   const user = userController.update(id, req.body);
   res.send(user);
 });
-router.delete("/:id", handleErrorAsync, (req, res) => {
+router.delete("/:id", isAuthenticated, (req, res) => {
   const { id } = req.params;
   userController.delete(id);
   res.status(204).send();
