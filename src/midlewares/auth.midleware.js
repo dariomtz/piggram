a8de3d3f56868388a21c3e8e5ebf01a81821e48b
miguel partida <ma.gonzalezp33@gmail.com>
2022-05-09 18:58:57 -0500

    routers handlers and like mixed

--- src/midlewares/auth.midleware.js
+++ src/midlewares/auth.midleware.js
@@ -2,7 +2,7 @@ function isAuthenticated(req, res, next) {
   if (req.user != null && req.user["passportID"] != null) {
     next();
   } else {
-    res.status(401).send({ error: "Not authorized" });
+    res.status(401).send({ error: "Not authorizedd" });
   }
 }
 
--- src/routes/like.route.js
+++ src/routes/like.route.js
@@ -1,32 +1,46 @@
 const router = require("express").Router();
 const {isAuthenticated} =require("../midlewares/auth.midleware");
 const Like = require("../controllers/like.controller");
+const {handleErrorAsync} = require("../utils/hof"); 
 
 router.use(isAuthenticated);
 
-router.get('/post/:postId',async (req,res)=>{
+router.get('/post/:postId', handleErrorAsync(async (req,res)=>{
     let {postId} = req.params;
     let likes = await Like.getByPost(postId)
     res.send(likes);
-})
+}))
 
-router.get('/user/:userId',async (req,res)=>{
+router.get('/user/:userId',handleErrorAsync(async (req,res)=>{
     let {userId} = req.params;
     let likes = await Like.getByUser(userId);
     res.send(likes);
     
-})
+}))
 
-router.post('/:postId',async (req,res)=>{
+router.get('/post/:postId/count',handleErrorAsync(async (req,res)=>{
+    let {postId} = req.params;
+    let count = await Like.getCountByPost(postId)
+    res.send({count});
+}))
+
+router.get('/:postId',handleErrorAsync(async (req,res)=>{
+    let {postId} = req.params;
+    let liked = await Like.exist(postId,req.user._id);
+    console.log(liked);
+    res.send(liked);
+}))
+
+router.post('/:postId',handleErrorAsync(async (req,res)=>{
     let {postId} = req.params;
     let like = await Like.add(postId,req.user._id);
     res.send(like);
-})
+}))
 
-router.delete('/:postId',async (req,res)=>{
+router.delete('/:postId',handleErrorAsync(async (req,res)=>{
     let {postId} = req.params;
     await Like.remove(postId, req.user._id);
     res.send();
-})
+}))
 
 module.exports = router;
